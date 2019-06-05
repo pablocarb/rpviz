23190cf10801f592f4941f827bb90bf05ed4f8e8
Anabadier <anaelle.badier@agroparistech.fr>
2019-06-05 18:19:58 +0200

    update

---
+++ file_html/network_viewer.js
@@ -0,0 +1,37 @@
+$(function() {
+  var cy = cytoscape({
+    container: $('#cy'),
+    elements:ntElements
+  ,
+  layout: {
+        name: 'cose'
+      },
+
+    style: [
+      {
+      selector: 'breadthfirst',
+      style: {
+          "background-color": '#80D0D0',
+          "label": "data(id)",
+          "font-size": "7px"
+          }
+      },
+      {selector: "node[category='reactions']",
+      style: {
+        'background-color': '#FA8072',
+        'shape': 'roundrectangle'
+         }},
+     {selector: "node[category='reactant']",
+     style: {
+       'background-color': '#52be80',
+        }},
+     {
+      selector: 'edge',
+      style: {
+        'curve-style': 'bezier',
+        'width': '3px',
+        'target-arrow-shape': 'triangle',
+      }
+    }]
+  });
+})
