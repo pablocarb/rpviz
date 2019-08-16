06791c0d4a5e8cf1ebd51a868e98a6ce7b37d540
Anabadier <anaelle.badier@agroparistech.fr>
2019-08-16 15:59:50 +0100

    Update tsv2name.py

--- tsv2name.py
+++ tsv2name.py
@@ -8,29 +8,23 @@ Created on Wed Aug  7 13:56:48 2019
 #import pandas as pd
 import cirpy
 import csv
+import pubchempy as pcp
 
-
-#def id2name(id):
-#    """for MNX compounds"""
-#    dataname=pd.read_csv('rpviz/chem_prop.tsv', sep="\t", skiprows=385)
-#    dataname = dataname.rename(columns={'#MNX_ID': 'MNX_ID'})
-#    try : 
-#        name=dataname.loc[dataname['MNX_ID']==id,'Description'].values[0] 
-#    except :
-#        name=id                                
-#    return(name)
-    
 
 #smiles='[H]OC(=O)C([H])(N([H])[H])C([H])([H])c1c([H])c([H])c(O[H])c(O[H])c1[H]'
 def smile2name(smiles,id,d):
     """for products compounds"""
-    try: 
-        name=d[id.split('_')[0]] #try if it's in metanetx DB
-    except:
-        try :
-            name=cirpy.query(smiles,'names')[0].value[0]
-        except :
-            name=id
+    try: #explore pubchem database
+        CID=pcp.get_compounds(smiles, 'smiles')[0].cid
+        name=pcp.Compound.from_cid(CID).synonyms[0]
+    except :
+        try: #explore metanetx database
+            name=d[id.split('_')[0]] #try if it's in metanetx DB
+        except:
+            try :#query cirpy database
+                name=cirpy.query(smiles,'names')[0].value[0]
+            except :
+                name=id
     return(name)
     
     
